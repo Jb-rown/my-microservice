42ab416d6b5900c845c7e882c4a9edf97b6b4ec1
Jb-rown <brownjohn9870@gmail.com>
2025-10-21 12:30:41 +0300

    add ESLint configuration for code quality

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-console": "off",
+    "no-trailing-spaces": "error",
+    "eol-last": "error",
+  },
+};
